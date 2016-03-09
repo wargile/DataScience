5c5f1c7c2725eb465cd273cfd9dd3aa9dff2b039
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2016-03-08 21:36:00 -0600

    update
    added files

--- DSiR01.R
+++ DSiR01.R
@@ -1,48 +1,4 @@
 
-options(digits = 2)
-
-txt = readLines("Data/offline.final.trace.txt")
-
-sum(substr(txt, 1, 1) == "#")
-
-length(txt)
-
-strsplit(txt[4], ";")[[1]]
-
-tokens = strsplit(txt[4], "[;=,]")[[1]]
-
-tokens[1:10]
-
-tokens[c(2, 4, 6:8, 10)]
-
-tokens[ - ( 1:10 ) ]
-
-tmp = matrix(tokens[ - (1:10) ], ncol = 4, byrow = TRUE)
-mat = cbind(matrix(tokens[c(2, 4, 6:8, 10)], nrow = nrow(tmp),
-                   ncol = 6, byrow = TRUE), 
-            tmp)
-
-dim(mat)
-
-processLine =
-function(x)
-{
-  tokens = strsplit(x, "[;=,]")[[1]]
-  tmp = matrix(tokens[ - (1:10) ], ncol = 4, byrow = TRUE)
-  cbind(matrix(tokens[c(2, 4, 6:8, 10)], nrow = nrow(tmp),
-               ncol = 6, byrow = TRUE), tmp)
-}
-
-tmp = lapply(txt[4:20], processLine)
-
-sapply(tmp, nrow)
-
-offline = as.data.frame(do.call("rbind", tmp))
-dim(offline)
-
-lines = txt[ substr(txt, 1, 1) != "#" ]
-tmp = lapply(lines, processLine)
-
 processLine = function(x)
 {
   tokens = strsplit(x, "[;=,]")[[1]]
